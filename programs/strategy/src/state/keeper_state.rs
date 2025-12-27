c52c63d59d262c40298000aff08e10c739bbb912
Abel Marnk <orinayobanji@gmail.com>
2025-12-27 19:08:33 +0100

    Add padding to keeper

--- programs/strategy/src/state/keeper_state.rs
+++ programs/strategy/src/state/keeper_state.rs
@@ -5,6 +5,7 @@ use anchor_lang::prelude::*;
 pub struct KeeperState {
     pub keeper: Pubkey,
     pub credits: u64,
+    pub padding: [u64; 7],
 }
 
 impl KeeperState{
@@ -14,6 +15,7 @@ impl KeeperState{
     ){
         self.keeper = *keeper;
         self.credits = 0u64;
+        self.padding = [0u64; 7];
     }
 
     pub fn reset_credits(&mut self){
@@ -25,6 +27,7 @@ impl KeeperState{
 fn test_keeper_state(){
     let _ = KeeperState{
         keeper:Pubkey::default(),
-        credits:u64::default()
+        credits:u64::default(),
+        padding:[0u64;7]
     };
 }
